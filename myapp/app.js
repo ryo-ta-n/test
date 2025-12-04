0109abb8b2e47f97caed30da246b6bc9d97d0e6f
ryota <ryouta1030nk@gmail.com>
2025-12-04 09:31:47 +0900

    commit

--- myapp/app.js
+++ myapp/app.js
@@ -10,7 +10,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 var helloRouter = require('./routes/hello');
-app.use('/hello', helloRouter);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/hello', helloRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
